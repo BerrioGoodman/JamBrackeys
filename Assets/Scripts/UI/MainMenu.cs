ccf056bc2478ee48fc212c038a4e502721b9f6ea
jpablorl87 <glcsmp@gmail.com>
2025-02-22 16:37:20 -0500

    Troubleshooting in UI

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,10 +6,12 @@ public class MainMenu : MonoBehaviour
     public void PlayButton()
     {
         SceneManager.LoadScene("Level1");
+        Time.timeScale = 1.0f;
     }
     public void CreditsButton()
     {
         SceneManager.LoadScene("Credits");
+        Time.timeScale = 1.0f;
     }
     public void QuitButton()
     {
@@ -18,5 +20,6 @@ public class MainMenu : MonoBehaviour
     public void BackButton()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1.0f;
     }
 }
